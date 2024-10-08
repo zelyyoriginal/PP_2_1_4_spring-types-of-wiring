d766e3bdb17e21e925713fce06433cd263cc66eb
Zelyyoriginal <danilazelenskij97@gmail.com>
2024-10-08 11:27:48 +0300

    complite

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,5 +1,7 @@
 package koschei.config;
 
+import koschei.models.Duck5;
+import koschei.models.Egg6;
 import koschei.models.Island2;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
@@ -13,5 +15,9 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+    @Bean
+    public static Duck5 getDuck(Egg6 egg) {
+        return new Duck5(egg);
     }
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,20 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class Egg6 {
+    private Needle7 needle;
+
+    @Autowired
+    public void setNeedle(Needle7 needle) {
+        this.needle = needle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
+
 }
